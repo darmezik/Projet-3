9c278d15574998b191367acda3178fd5c95457bc
darmezik <killiandarmezin@gmail.com>
2020-01-22 14:36:59 +0100

    app -> poo

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,50 @@
-var map = L.map('map').setView([45.7578137, 4.8320114], 11);
+class App {
+    constructor(mapId, mapCenter){
+        this.map = this.setMap(mapId, mapCenter)
+        this.initIcon();
+    }
+    setMap(mapId, mapCenter){
+        var map = L.map(mapId).setView(mapCenter, 10);
+        L.tileLayer('https://wxs.ign.fr/{apikey}/geoportail/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}', {
+            maxZoom: 19,
+            apikey: 'choisirgeoportail',
+	        format: 'image/jpeg',
+	        style: 'normal'
+        }).addTo(map);
+        return map
+    }
+    initIcon(){
+        var Icon = L.Icon.extend({
+            options:{
+                iconSize: [20,40]
+            }
+        });
+        var greenIcon = new Icon ({ iconUrl: 'public/images/markerGreen.png'});
+        var yellowIcon = new Icon ({ iconUrl: 'public/images/markerYellow.png'});
+        var redIcon = new Icon ({ iconUrl: 'public/images/markerRed.png'});
+        $.getJSON("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=17a48063730caa425f3a988c52e168ce3bf63007", function( data ){
+            var stations = [];
+            $.each(data, function(key, station){
+                stations.push(station);
+                if(station.available_bikes > 2){
+                    var icon = greenIcon;
+                }else if(station.available_bikes > 0){
+                    var icon = yellowIcon;
+                }else{
+                    var icon = redIcon;
+                }
+                var marker = L.marker([station.position.lat, station.position.lng], {icon: icon}).openPopup().addTo(this.map)
+                marker.on("click", function(e){
+                    document.getElementById("stationI").style.display = "block";
+                    document.getElementById("stationAdress").innerHTML = station.address;
+                    document.getElementById("bikesStand").innerHTML = station.bike_stands;
+                    document.getElementById("availableBikes").innerHTML = station.available_bikes;
+                })
+            }.bind(this))
+        }.bind(this));
+    }
+}
+/*var map = L.map('map').setView([45.7578137, 4.8320114], 11);
 L.tileLayer('https://wxs.ign.fr/{apikey}/geoportail/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',{
     maxZoom: 19,
     apikey: 'choisirgeoportail',
@@ -48,30 +94,5 @@ $.getJSON("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=17a4806
         }
         
     });
-});
+});*/
 
-/* class App {
-    constructor(mapId, mapCenter){
-        this.map = this.setMap(mapId, mapCenter)
-    }
-    setMap(mapId, mapCenter){
-        var map = L.map(mapId).setView(mapCenter, 10);
-        L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-            maxZoom: 20
-        }).addTo(map);
-        return map
-    }
-    async setMarkersFromApi(url){
-        const parsedResults = await this.retrieveContent(url);
-        console.log("parsedResults", parsedResults);
-        for(const accessPoint of parsedResults){
-            L.marker(accessPoint.position.lat.lng).addTo(this.map)
-                .bindPopup(accessPoint.name)
-                .openPopup();
-        }
-    }
-    async retrieveContent(url){
-        const response = await fetch(url);
-        return response.json();
-    }
-} */
--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,1 @@
-let app = New App( mapId: 'map', mapCenter: [45.7578137, 4.8320114] )
-app.setMarkersFromApi('https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=17a48063730caa425f3a988c52e168ce3bf63007')
+let app = new App('map', [45.7578137, 4.8320114])
